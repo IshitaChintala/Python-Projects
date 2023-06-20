c3a40fbf5609c9d64a03a9c39086c1001381bf3b
Ishita Chintala <70445943+IshitaChintala@users.noreply.github.com>
2023-06-20 11:21:48 -0400

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 17:53:58 2023
+
+@author: ishita
+"""
+weight = 150 #kg
+height = 1.57 #meters
+bmi = weight / (height**2)
+print (bmi)
+
+#need to loose weight since i'm overweight!!
+
